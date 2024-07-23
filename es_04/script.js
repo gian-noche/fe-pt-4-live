960baa8e3c33e8b83e8131865a8c8d44111359f6
Gian Levi Noche <gian.noche@gmail.com>
2024-07-23 20:36:05 +0200

    Ripasso js

--- es_04/script.js
+++ es_04/script.js
@@ -1,5 +1,29 @@
-const items = document.getElementById("itemsP")
+//const items = document.getElementById("itemsP")
 
-console.log(items)
+//console.log(items)
 
 // Creare un array
+const arr = ["item1", "item2", "item3", "item4", "item5"]
+
+// Creare gli elementi dinamicamente
+//items.innerHTML = '<p class="blank">Pippo</p>'
+
+
+// METODO 2: Ciclare l'array con il metodo .for loop
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    switch (element) {
+        case "item2": 
+            console.log("Gatto")
+            break;
+
+        case "item5":
+            console.log("Cavallo")
+            break;
+    
+        default: 
+            console.log("Default")
+            break;
+    } 
+}
+
